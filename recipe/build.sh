c59d0883f597dfdb46807d3dfb72e2f2d8dbe749
Ray Donnelly <mingw.android@gmail.com>
2017-09-04 22:39:38 +0000

    Set CC_FOR_BUILD

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,14 @@ fi
 if [ `uname` == Linux ]; then
     export HELP2MAN=/bin/true
 fi
-./configure --prefix="$PREFIX" --host=${HOST}
+if [[ ${HOST} =~ .*linux.* ]]; then
+    export CC=${GCC}
+    # TODO :: Handle cross-compilation properly here
+    export CC_FOR_BUILD=${GCC}
+fi
+./configure --prefix="$PREFIX"  \
+            --host=${HOST}      \
+            --build=${BUILD}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 # CXX restart fails?
 # make check
